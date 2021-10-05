94c309d6ad52cb9ce09294fd102d56ff1e7cd92d
ctc <86436966+praecantat0r@users.noreply.github.com>
2021-10-05 20:08:10 +0200

    Create hello_world.asm

---
+++ hello_world.asm
@@ -0,0 +1,24 @@
+; Hello World by ctc
+
+
+global _start
+
+_start:
+ jmp short ender
+ starter:
+ xor eax, eax 
+ xor ebx, ebx 
+ xor edx, edx 
+ xor ecx, ecx 
+ mov al, 4 
+ mov bl, 1 
+ pop ecx 
+ mov dl, 18 
+ int 0x80 
+ xor eax, eax 
+ mov al, 1 
+ xor ebx,ebx
+ int 0x80
+ ender:
+ call starter 
+ db 'Hello World!'
